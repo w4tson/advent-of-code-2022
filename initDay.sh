fe686c97477a5b899763e86ade6d979faceccea6
watson <paul@hashbang.digital>
2022-12-03 20:35:19 +0000

    emojis

--- initDay.sh
+++ initDay.sh
@@ -5,18 +5,18 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
 SRC_DIR=$SCRIPT_DIR/lib/src/main/kotlin/aoc
 TEST_DIR=$SCRIPT_DIR/lib/src/test/kotlin/aoc
-TEST_RES_DIR=$SCRIPT_DIR/lib/src/test/resources/aoc
+TEST_RES_DIR=$SCRIPT_DIR/lib/src/test/resources
 
 prev_day=`ls $SRC_DIR | grep -E "day[0-9]+" | sort | tail -n 1 | sed 's/day//'`
 
 printf -v next_day "%02d" $(($prev_day+1))
-echo $next_day
+echo "Generating Day$next_day..."
 
 mkdir -p $SRC_DIR/day$next_day
 mkdir -p $TEST_DIR/day$next_day
 mkdir -p $TEST_RES_DIR/day$next_day
-touch $TEST_RES_DIR/day${next_day}.txt
-touch $TEST_RES_DIR/test.txt
+touch $TEST_RES_DIR/day${next_day}/day${next_day}.txt
+touch $TEST_RES_DIR/day${next_day}/day${next_day}-test.txt
 
 cat << EOF > $SRC_DIR/day${next_day}/Day${next_day}.kt
 package aoc.day${next_day}
@@ -27,7 +27,7 @@ fun part1(input : String) {
 EOF
 
 cat << EOF > $TEST_DIR/day${next_day}/Day${next_day}Test.kt
-package aoc.day03
+package aoc.day${next_day}
 
 import Util.Companion.readInput
 import org.junit.jupiter.api.Assertions.*
@@ -38,17 +38,20 @@ import org.junit.jupiter.api.Test
 class Day${next_day}Test {
 
     val input = readInput("/day${next_day}/day${next_day}.txt")
+    val testInput = readInput("/day${next_day}/day${next_day}-test.txt")
 
-    val test = """
+    val testStr = """
         ...
     """.trimIndent()
 
     @Test
     fun test1() {
-        part1(test)
+        part1(testStr)
     }
 
 }
 
 EOF
+
+echo "DONE \U0001f381 \U0001f384 \U0001f385"
 
